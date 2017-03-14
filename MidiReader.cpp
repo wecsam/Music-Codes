6adb16973dd38f940bb73921e1900e531e7ce01a
wecsam <david@davidtsai.net78.net>
2017-03-13 23:39:32 -0400

    Added ability to read some meta events in an MIDI track

--- MidiReader.cpp
+++ MidiReader.cpp
@@ -3,10 +3,11 @@
 #include "MidiReader.h"
 using namespace std;
 namespace MusicCodes {
-	MidiReader::MidiReader(istream& input) : input(input) {
+	// MidiReader
+	MidiReader::MidiReader(istream& input) : input(input), currentTrack(NULL) {
 		// Make sure that this is a MIDI file.
 		midiValid = false;
-		// Read the first four bytes and check for the MIDI header chunk.
+		// Read the first four bytes and check for the beginning of a MIDI header chunk.
 		char buffer[5];
 		input.read(buffer, 4);
 		buffer[4] = 0;
@@ -29,6 +30,9 @@ namespace MusicCodes {
 			}
 		}
 	}
+	MidiReader::~MidiReader(){
+		delete currentTrack;
+	}
 	MidiReader::operator bool() const {
 		return midiValid;
 	}
@@ -38,6 +42,24 @@ namespace MusicCodes {
 		input.read(buffer, sizeof(T));
 		reverse(buffer, buffer + sizeof(T));
 		return *(T*)buffer;
+	}
+	unsigned int MidiReader::getVariableLengthValue(){
+		unsigned int result = 0;
+		unsigned char nextByte;
+		do {
+			nextByte = input.get();
+			// Copy the lower 7 bits into result.
+			// MIDI stores the values in the lower 7 bits of each byte.
+			// This bitshift essentially concatenates groups of seven bits.
+			result = (result << 7) | (nextByte & 0x7F);
+			// If the most significant bit is 0, then this is the least significant byte.
+			// if this is the last significant byte, then stop reading more bytes; they
+			// are not a part of this variable-length value.
+		} while(nextByte & 0x80);
+		return result;
+	}
+	void MidiReader::skipBytes(streamoff n){
+		input.seekg(n, ios_base::cur);
 	}
 	ostream& operator<<(ostream& lhs, const MidiReader& rhs){
 		lhs << "<MidiReader: valid=" << rhs.midiValid << ", format=";
@@ -56,5 +78,145 @@ namespace MusicCodes {
 		}
 		lhs << ", tracks=" << rhs.midiNumTracks << ", division=" << rhs.midiDivision << ">";
 		return lhs;
+	}
+	// MidiReader::Track
+	MidiReader::Track::Track(MidiReader* file)
+	: file(file), sequenceNumber(0), lastSeenTempo(500000), lastSeenTimeSignature(NULL)
+	{
+		// Make sure that this is a MIDI track.
+		// Read the first four bytes and check for the beginning of a MIDI track.
+		char buffer[5];
+		file->input.read(buffer, 4);
+		buffer[4] = 0;
+		trackValid = (strcmp(buffer, "MTrk") == 0);
+		if(trackValid){
+			// Read the length of the MIDI track.
+			lengthMTrk = file->getValue<uint32_t>();
+			// Remember the current position of the istream.
+			// When (file->input.tellg() - streamPositionStart) == lengthMTrk,
+			// we have reached the end of the data for this track.
+			streamPositionStart = file->input.tellg();
+		}
+	}
+	MidiReader::Track::~Track(){
+		delete lastSeenTimeSignature;
+		delete lastSeenKeySignature;
+	}
+	MidiReader::Track::operator bool() const {
+		return trackValid;
+	}
+	MidiReader::Track::EVENT MidiReader::Track::handleNextEvent(){
+		// Get the amount of time since the last event. Remember, MIDI uses a unit
+		// of time called a delta, and the actual duration of a delta is defined in
+		// the MIDI header. It can be referenced as file->midiDivision.
+		unsigned int timeSinceLastEvent = file->getVariableLengthValue();
+		// Read the next byte, which indicates the type of the event.
+		switch(file->input.get()){
+			case 0xFF: {
+				// Read the next byte, which indicates the type of meta.
+				unsigned char metaType = file->input.get();
+				// The next data is the length of the meta event.
+				unsigned int metaLength = file->getVariableLengthValue();
+				// Although there are many possible types of meta events,
+				// only some are handled below.
+				switch(metaType){
+					case 0x00:
+						// Sequence Number
+						// The length should be two bytes.
+						if(metaLength == sizeof(uint16_t)){
+							// The length is correct.
+							sequenceNumber = file->getValue<uint16_t>();
+						}else{
+							// The length is incorrect.
+							file->skipBytes(metaLength);
+						}
+						break;
+					case 0x03: {
+						// Sequence/Track Name
+						// Read next metaLength bytes as a string.
+						char buffer[metaLength + 1];
+						file->input.read(buffer, metaLength);
+						buffer[metaLength] = 0;
+						// Invoke string& operator= (const char* s).
+						name = buffer;
+						break;
+					}
+					case 0x51:
+						// Tempo
+						// The length should be three bytes.
+						if(metaLength == 3){
+							// The length is correct.
+							// Copy each byte into lastSeenTempo in Big Endian order.
+							*((char*)&lastSeenTempo) = 0;
+							*((char*)&lastSeenTempo + 1) = file->input.get();
+							*((char*)&lastSeenTempo + 2) = file->input.get();
+							*((char*)&lastSeenTempo + 3) = file->input.get();
+						}else{
+							// The length is incorrect.
+							file->skipBytes(metaLength);
+						}
+						break;
+					case 0x58:
+						// Time Signature
+						// The length should be four bytes.
+						if(metaLength == 4){
+							// The length is correct.
+							// Destroy the last time signature.
+							delete lastSeenTimeSignature;
+							// Save the new time signature. The constructor reads four bytes.
+							lastSeenTimeSignature = new TimeSignature(file->input);
+						}else{
+							// The length is incorrect.
+							file->skipBytes(metaLength);
+						}
+						break;
+					case 0x59:
+						// Key Signature
+						// The length should be two bytes.
+						if(metaLength == 2){
+							// The length is correct.
+							// Destroy the last time signature.
+							delete lastSeenKeySignature;
+							// Save the new time signature. The constructor reads two bytes.
+							lastSeenKeySignature = new KeySignature(file->input);
+						}else{
+							// The length is incorrect.
+							file->skipBytes(metaLength);
+						}
+						break;
+					default:
+						// We are not interested in this type of meta event.
+						// Just scan to the end of the message.
+						file->skipBytes(metaLength);
+				}
+				return META_EVENT;
+			}
+			case 0xF0:
+			case 0xF7:
+				// We're not doing anything with system exclusive events for now.
+				// Scan until the End-of-Exclusive event.
+				while(file->input.get() != 0xF7);
+				return SYSEX_EVENT;
+		}
+		if(timeSinceLastEvent == 0){
+			
+		}
+		// Return NUM_EVENTS to indicate that the event type was unknown.
+		return NUM_EVENTS;
+	}
+	bool MidiReader::Track::endOfData() const {
+		return (file->input.tellg() - streamPositionStart) == lengthMTrk;
+	}
+	// MidiReader::Track::TimeSignature
+	MidiReader::Track::TimeSignature::TimeSignature(istream& is){
+		numerator = is.get();
+		denominatorLog2 = is.get();
+		clocksPerMetronomeTick = is.get();
+		demisemiquaversPer24Clocks = is.get();
+	}
+	// MidiReader::Track::KeySignature
+	MidiReader::Track::KeySignature::KeySignature(istream& is){
+		numSharpsFlats = is.get();
+		minor = is.get();
 	}
 }
--- MidiReader.h
+++ MidiReader.h
@@ -5,10 +5,14 @@
 	Some excellent MIDI references:
 	http://www.ccarh.org/courses/253/handout/smf/
 	http://cs.fit.edu/~ryan/cse4051/projects/midi/midi.html
+	
+	For MIDI note numbers:
+	http://www.electronics.dit.ie/staff/tscarff/Music_technology/midi/midi_note_numbers_for_octaves.htm
 */
 #ifndef INCLUDE_MUSIC_CODES_MIDIREADER
 #define INCLUDE_MUSIC_CODES_MIDIREADER 1
 #include <iostream>
+#include <string>
 #include "Note.h"
 namespace MusicCodes {
 	class MidiReader {
@@ -16,9 +20,70 @@ namespace MusicCodes {
 	public:
 		MidiReader(std::istream& input);
 		// TODO: copy and move constructors
+		~MidiReader();
 		MusicCodes::Note getNextNote();
 		operator bool() const;
 		enum FORMAT { SINGLE_TRACK, MULTI_TRACK, MULTI_SONG, NUM_FORMATS };
+		class Track {
+		public:
+			Track(MidiReader*);
+			~Track();
+			operator bool() const;
+			enum EVENT { MIDI_EVENT, META_EVENT, SYSEX_EVENT, NUM_EVENTS };
+			// Returns the type of event that was handled. If the return value is NUM_EVENTS,
+			// then the event type was unknown. It is assumed that trackValid is true.
+			EVENT handleNextEvent();
+			// Returns whether the end of the data for this track has been reached.
+			// When this is true, do not call handleNextEvent().
+			bool endOfData() const;
+			class TimeSignature {
+			public:
+				// Construct a new time signature. Pass in a reference to the MIDI file.
+				// The next four bytes will be read and interpreted as the time signature.
+				TimeSignature(std::istream&);
+			private:
+				// The time signature's numerator. Example: the numerator of 6/8 is 6.
+				uint8_t numerator;
+				// The log base 2 of the signature's denominator.
+				// Example: the log base 2 of the denominator of 6/8 is 3 because log_2(8)=3.
+				uint8_t denominatorLog2;
+				// The number of MIDI clocks per metronome tick.
+				uint8_t clocksPerMetronomeTick;
+				// The number of 32nd notes per 24 MIDI clocks
+				uint8_t demisemiquaversPer24Clocks;
+			};
+			class KeySignature {
+			public:
+				// Construct a new key signature. Pass in a reference to the MIDI file.
+				// The next two bytes will be read and interpreted as the key signature.
+				KeySignature(std::istream&);
+			private:
+				// The number of sharps or flats in the key signature.
+				// A negative number indicates flats. A positive number indicates sharps.
+				int8_t numSharpsFlats;
+				// Minor if true. Major if false.
+				bool minor;
+			};
+		private:
+			bool trackValid;
+			// A pointer to the containing MidiReader
+			MidiReader* file;
+			// The length of the track data
+			uint32_t lengthMTrk;
+			// The stream position of the istream right after the length field
+			std::streampos streamPositionStart;
+			// The sequence number of the track
+			uint16_t sequenceNumber;
+			// The name of the track
+			std::string name;
+			// The tempo that was last seen
+			// The tempo is expressed as the number of microseconds per quarter note.
+			uint32_t lastSeenTempo;
+			// The time signature that was last seen
+			TimeSignature* lastSeenTimeSignature;
+			// The key signature that was last seen
+			KeySignature* lastSeenKeySignature;
+		};
 	private:
 		std::istream& input;
 		// Whether the input istream contained a valid MIDI header
@@ -31,8 +96,14 @@ namespace MusicCodes {
 		uint16_t midiNumTracks;
 		// The unit of time that this MIDI file uses, as ticks per beat
 		int16_t midiDivision;
+		// The MIDI track that is currently being read (NULL if no track is being read)
+		Track* currentTrack;
 		// Reads the next sizeof(T) bytes from the istream and returns them as type T
 		template <class T> T getValue();
+		// Reads a variable-length value from the istream and returns it
+		unsigned int getVariableLengthValue();
+		// Skips over n bytes in the istream
+		void skipBytes(std::streamoff n);
 	};
 }
 #endif
