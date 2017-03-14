ed25978f31db60d64bc529bf98ae8f39729ebb45
wecsam <david@davidtsai.net78.net>
2017-03-13 23:17:57 -0400

    Added dotted notes and Note output function

--- Note.cpp
+++ Note.cpp
@@ -1,12 +1,70 @@
+#include <cmath>
 #include "Note.h"
 namespace MusicCodes {
-	Note::Note(uint8_t pitch, int8_t duration) : pitch(pitch), duration(duration) {}
-	Note::Note(const Note& rhs) : pitch(rhs.pitch), duration(rhs.duration) {}
-	Note::Note(Note&& rhs) : pitch(rhs.pitch), duration(rhs.duration) {}
+	Note::Note(uint8_t pitch, int duration, int dots) : pitch(pitch), duration(duration), dots(dots) {}
 	uint8_t Note::getPitch() const {
 		return pitch;
 	}
-	int8_t Note::getDuration() const {
+	int Note::getDuration() const {
 		return duration;
+	}
+	int Note::getDots() const {
+		return dots;
+	}
+	const std::string Note::NOTE_NAMES[] = {
+		"C",
+		"C#",
+		"D",
+		"D#",
+		"E",
+		"F",
+		"F#",
+		"G",
+		"G#",
+		"A",
+		"A#",
+		"B"
+	};
+	std::ostream& operator<<(std::ostream& os, const Note& rhs){
+		os << Note::NOTE_NAMES[rhs.pitch % 12] << rhs.pitch / 12 << ' ';
+		switch(rhs.dots){
+			case 0:
+				break;
+			case 1:
+				os << "dotted ";
+				break;
+			case 2:
+				os << "double-dotted ";
+				break;
+			case -1:
+				os << "negative-dotted ";
+				break;
+			default:
+				os << rhs.dots << "-times-dotted ";
+		}
+		switch(-rhs.duration){
+			case 0:
+				os << "whole";
+				break;
+			case 1:
+				os << "half";
+				break;
+			case 2:
+				os << "quarter";
+				break;
+			case 3:
+				os << "eighth";
+				break;
+			case 4:
+				os << "sixteenth";
+				break;
+			case 5:
+				os << "thirty-second";
+				break;
+			default:
+				os << pow(2, -rhs.duration) << "th";
+		}
+		os << " note";
+		return os;
 	}
 }
--- Note.h
+++ Note.h
@@ -4,20 +4,28 @@
 #ifndef INCLUDE_MUSIC_CODES_NOTE
 #define INCLUDE_MUSIC_CODES_NOTE 1
 #include <cstdint>
+#include <iostream>
+#include <string>
 namespace MusicCodes {
 	class Note {
+		friend std::ostream& operator<<(std::ostream&, const Note&);
 	public:
-		Note(uint8_t pitch, int8_t duration);
-		Note(const Note& rhs);
-		Note(Note&& rhs);
+		Note(uint8_t pitch, int duration, int dots);
 		uint8_t getPitch() const;
-		int8_t getDuration() const;
+		int getDuration() const;
+		int getDots() const;
+		// An array of note names
+		static const std::string NOTE_NAMES[];
 	private:
 		// The MIDI pitch number of this note
 		uint8_t pitch;
 		// The duration of this note, expressed as an exponent of 2.
+		// The duration is multiplied by 1.5 for every dot.
 		// For example, for a quarter note, duration=-2 because 2^(-2)=1/4.
-		int8_t duration;
+		// For example, for a dotted quarter note, duration=-2 and dots=1.
+		// For example, for a double-dotted whole note, duration=0 and dots=2.
+		int duration;
+		int dots;
 	};
 }
 #endif
